ced0e44fd9b899da113260ab051d38c3439166d9
ConnectedDot <psalmomo@gmail.com>
2023-04-15 20:34:46 +0100

    Add: Fixed the Import and export from sanity deps

--- sanity-ecomms/sanity.cli.js
+++ sanity-ecomms/sanity.cli.js
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'yjbgm69e',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
