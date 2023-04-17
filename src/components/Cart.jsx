7944a464afa71d0c2780de537018e89c3f95546c
ConnectedDot <psalmomo@gmail.com>
2023-04-17 21:31:18 +0100

    Add: Slug in the project folder

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -7,11 +7,10 @@ import {
   AiOutlineShopping,
 } from 'react-icons/ai';
 import { TiDeleteOutline } from 'react-icons/ti';
-import toast from 'react-hot-toast';
+import { toast } from 'react-hot-toast';
 
-// import { useStateContext } from '../context/StateContext';
+import { useStateContext } from '../context/StateContext';
 import { urlFor } from '../lib/client';
-import getStripe from '../lib/getStripe';
 
 const Cart = () => {
   const cartRef = useRef();
@@ -20,29 +19,7 @@ const Cart = () => {
     totalQuantities,
     cartItems,
     setShowCart,
-    toggleCartItemQuanitity,
-    onRemove,
   } = useStateContext();
-
-  const handleCheckout = async () => {
-    const stripe = await getStripe();
-
-    const response = await fetch('/api/stripe', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(cartItems),
-    });
-
-    if (response.statusCode === 500) return;
-
-    const data = await response.json();
-
-    toast.loading('Redirecting...');
-
-    stripe.redirectToCheckout({ sessionId: data.id });
-  };
 
   return (
     <div className="cart-wrapper" ref={cartRef}>
@@ -67,12 +44,11 @@ const Cart = () => {
                 onClick={() => setShowCart(false)}
                 className="btn"
               >
-                Continue Shopping
+                Continue shopping
               </button>
             </Link>
           </div>
         )}
-
         <div className="product-container">
           {cartItems.length >= 1 &&
             cartItems.map((item) => (
@@ -81,40 +57,28 @@ const Cart = () => {
                   src={urlFor(item?.image[0])}
                   className="cart-product-image"
                 />
+
                 <div className="item-desc">
-                  <div className="flex top">
+                  <div className="flex-top">
                     <h5>{item.name}</h5>
                     <h4>${item.price}</h4>
                   </div>
-                  <div className="flex bottom">
+                  <div className="flex-bottom">
                     <div>
                       <p className="quantity-desc">
-                        <span
-                          className="minus"
-                          onClick={() =>
-                            toggleCartItemQuanitity(item._id, 'dec')
-                          }
-                        >
+                        <span className="minus" onClick="">
                           <AiOutlineMinus />
                         </span>
                         <span className="num" onClick="">
-                          {item.quantity}
+                          0
                         </span>
-                        <span
-                          className="plus"
-                          onClick={() =>
-                            toggleCartItemQuanitity(item._id, 'inc')
-                          }
-                        >
+                        <span className="plus" onClick="">
                           <AiOutlinePlus />
                         </span>
                       </p>
                     </div>
-                    <button
-                      type="button"
-                      className="remove-item"
-                      onClick={() => onRemove(item)}
-                    >
+
+                    <button type="button" className="remove-item" onClick="">
                       <TiDeleteOutline />
                     </button>
                   </div>
@@ -122,6 +86,7 @@ const Cart = () => {
               </div>
             ))}
         </div>
+
         {cartItems.length >= 1 && (
           <div className="cart-bottom">
             <div className="total">
@@ -129,7 +94,7 @@ const Cart = () => {
               <h3>${totalPrice}</h3>
             </div>
             <div className="btn-container">
-              <button type="button" className="btn" onClick={handleCheckout}>
+              <button type="button" className="btn" onClick="">
                 Pay with Stripe
               </button>
             </div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Toaster } from 'react-hot-toast';
 import { Layout } from '../components';
 import './styles/globals.css';
+import { StateContext } from '../../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Toaster />
-      <Component {...pageProps} />
-    </Layout>
-    // {/* </StateContext> */}
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   );
 }
 
